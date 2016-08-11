edb0a5e72dbe24c2cfd6464465342106a526d961
Jurasikt <tsykun314@gmail.com>
2016-08-11 22:55:21 +0300

    fix todo: hardcode vendor name

---
+++ application/classes/Request/Client/Internal.php
@@ -0,0 +1,131 @@
+<?php
+
+use Kernel\Kernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Request_Client_Internal extends Kohana_Request_Client_Internal
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function execute_request(Request $request, Response $response)
+    {
+        // Create the class suffix
+        $suffix = 'Controller';
+
+        // Controller
+        $controller = $request->controller();
+
+        /*$container = $this->getContainer();
+        $um = $container->get('ovpn_user.manager');*/
+
+        $classController = null;
+        foreach ($this->getBundlesName() as $bundleName) {
+            if (class_exists($bundleName . '\\Controller\\' . $controller . $suffix)) {
+                $classController = $bundleName . '\\Controller\\' . $controller . $suffix;
+                break;
+            }
+        }
+
+        if (Kohana::$profiling) {
+            // Set the benchmark name
+            $benchmark = '"'.$request->uri().'"';
+
+            if ($request !== Request::$initial and Request::$current) {
+                // Add the parent request uri
+                $benchmark .= ' « "'.Request::$current->uri().'"';
+            }
+
+            // Start benchmarking
+            $benchmark = Profiler::start('Requests', $benchmark);
+        }
+
+        // Store the currently active request
+        $previous = Request::$current;
+
+        // Change the current request to this request
+        Request::$current = $request;
+
+        // Is this the initial request
+        //$initial_request = ($request === Request::$initial);
+
+        try {
+            if ( !$classController) {
+
+                throw HTTP_Exception::factory(404,
+                    'The requested URL :uri was not found on this server.',
+                    array(':uri' => $request->uri())
+                )->request($request);
+            }
+
+            // Load the controller using reflection
+            $class = new ReflectionClass($classController);
+
+            if ($class->isAbstract()) {
+                throw new Kohana_Exception(
+                    'Cannot create instances of abstract :controller',
+                    array(':controller' => $classController)
+                );
+            }
+
+            // Create a new instance of the controller
+            $controller = $class->newInstance($request, $response);
+
+            // Run the controller's execute() method
+            $response = $class->getMethod('execute')->invoke($controller);
+
+            if ( ! $response instanceof Response) {
+                // Controller failed to return a Response.
+                throw new Kohana_Exception('Controller failed to return a Response');
+            }
+        }
+
+        catch (HTTP_Exception $e) {
+            // Store the request context in the Exception
+            if ($e->request() === NULL) {
+                $e->request($request);
+            }
+
+            // Get the response via the Exception
+            $response = $e->get_response();
+        }
+
+        catch (Exception $e) {
+            // Generate an appropriate Response object
+            $response = Kohana_Exception::_handler($e);
+        }
+
+        // Restore the previous request
+        Request::$current = $previous;
+
+        if (isset($benchmark)) {
+            // Stop the benchmark
+            Profiler::stop($benchmark);
+        }
+
+        // Return the response
+        return $response;
+    }
+
+    /**
+     * @return ContainerBuilder
+     */
+    private function getContainer()
+    {
+        return Kernel::getContainer();
+    }
+
+    /**
+     * @return array
+     */
+    private function getBundlesName()
+    {
+        $names = [];
+        foreach (Kernel::getBundles() as $bundle) {
+            $names[] = $bundle->getName();
+        }
+
+        return $names;
+    }
+}
