f578685f5e8bd4e6d9cf2ffffe25486810573d2b
Vladimir Tsykun <Vladimir.Tsykun@effective-soft.com>
2016-08-19 11:33:50 +0300

    add test for security facade

--- src/Ovpn/Tests/Unit/SecurityFacadeTest.php
+++ src/Ovpn/Tests/Unit/SecurityFacadeTest.php
@@ -1,29 +1,50 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: jurasikt
- * Date: 18.8.16
- * Time: 16.52
- */
-class SecurityFacadeTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+use Ovpn\Security\SecurityFacade;
+
+class SecurityFacadeTest extends TestCase
 {
-    /**
-     * @var \ReflectionClass
-     */
-    public $reflect;
+
+    protected $token;
+
+    protected $abstractUser;
+
+    protected $tokenStorage;
+
+    protected $security;
+
+    protected $authorization;
 
     public function setUp()
     {
-        $this->reflect = new \ReflectionClass('Ovpn\Security\SecurityFacade');
-        $this->reflect->getProperty('security')->setAccessible(true);
-        $this->reflect->getProperty('authorization')->setAccessible(true);
-        $this->reflect->getProperty('tokenStrategy')->setAccessible(true);
+        $this->token = $this->createMock('Ovpn\Security\TokenStorageInterface');
+        $this->abstractUser = $this->createMock('Ovpn\Entity\UsersInterface');
+        $this->authorization = $this->createMock('Ovpn\Security\Authorization');
+        $this->security = $this->createMock('Ovpn\Security\Security');
+        $this->tokenStorage = $this->createMock('Ovpn\Security\TokenStorage');
     }
 
     public function testGetUser()
     {
-        $facade = $this->reflect->newInstanceWithoutConstructor();
-        $this->reflect;
+        $this->tokenStorage->expects($this->once())
+            ->method('getTokens')
+            ->willReturn(array_fill(0, 3, $this->token));
+        $this->security->expects($this->once())
+            ->method('setTokenStorage');
+        $this->security->expects($this->once())
+            ->method('getAbstractUser')
+            ->willReturn($this->abstractUser);
+
+        $facade = new SecurityFacade($this->authorization, $this->security, $this->tokenStorage);
+        $this->assertSame($facade->getUser(), $this->abstractUser);
+    }
+
+    public function testDoLogin()
+    {
+        $this->authorization->expects($this->once())
+            ->method('doLogin');
+        $facade = new SecurityFacade($this->authorization, $this->security, $this->tokenStorage);
+        $facade->doLogin('email','password');
     }
 }
