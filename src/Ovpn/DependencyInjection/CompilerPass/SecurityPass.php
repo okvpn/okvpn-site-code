c3b5c7b31a4afa78778a7089326560b852626dfe
Vladimir Tsykun <Vladimir.Tsykun@effective-soft.com>
2016-08-22 15:35:30 +0300

    add priority for load secure.token tags

--- src/Ovpn/DependencyInjection/CompilerPass/SecurityPass.php
+++ src/Ovpn/DependencyInjection/CompilerPass/SecurityPass.php
@@ -21,9 +21,29 @@ class SecurityPass implements CompilerPassInterface
         if ($container->has($this->name)) {
             $definition = $container->getDefinition($this->name);
 
-            foreach ($container->findTaggedServiceIds($this->tagName) as $id => $name) {
-                $definition->addMethodCall('addToken', [new Reference($id)]);
+            foreach ($container->findTaggedServiceIds($this->tagName) as $id => $args) {
+                $def = $container->getDefinition($id);
+                if ($def->isPublic()) {
+                    throw new \InvalidArgumentException(sprintf('The service "%s" must be not public.', $id));
+                }
+
+                if ($def->isAbstract()) {
+                    throw new \InvalidArgumentException(sprintf('The service "%s" must not be abstract.', $id));
+                }
+
+                foreach ($args as $name) {
+                    if (array_key_exists('priority', $name)) {
+                        $priority = $name['priority'];
+                    } else {
+                        throw new \InvalidArgumentException(
+                            sprintf('Service "%s" must define the "priority" attribute on "%s" tags.', $id, $this->tagName));
+                    }
+
+                    $definition->addMethodCall('addToken', [new Reference($id), $priority]);
+                }
             }
+
+            $definition->addMethodCall('compile', []);
         }
     }
 }
--- src/Ovpn/Security/TokenStorage.php
+++ src/Ovpn/Security/TokenStorage.php
@@ -5,13 +5,27 @@ namespace Ovpn\Security;
 class TokenStorage extends \SplObjectStorage
 {
 
-    public function addToken(TokenInterface $token)
+    protected $tokens;
+
+    public function addToken(TokenInterface $token, $priority)
     {
-        $this->attach($token);
+        if (array_key_exists($priority, $this->tokens)) {
+            throw new \InvalidArgumentException('priority must be unequal for tags secure.token');
+        }
+
+        $this->tokens[$priority] = $token;
     }
 
     /**
      * todo: use spl iterator
      */
     public function getToken(){}
+
+    public function compile()
+    {
+        ksort($this->tokens);
+        foreach ($this->tokens as $token) {
+            $this->attach($token);
+        }
+    }
 }
