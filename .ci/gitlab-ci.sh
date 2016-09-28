f415316da25b9a9b64a4f0fede172335fe317049
Jurasikt <tsykun314@gmail.com>
2016-09-28 03:20:10 +0300

    fix error in parameters.php

--- .ci/gitlab-ci.sh
+++ .ci/gitlab-ci.sh
@@ -12,9 +12,9 @@ case $step in
         sed -i "s/name"\:".*/name"\:" $DB_NAME/g" application/phinx.yml;
         sed -i "s/pass"\:".*/pass"\:" 123456/g" application/phinx.yml;
         sed -i "s/user"\:".*/user"\:" okvpn/g" application/phinx.yml;
-        sed -i "s/'username'\s"\=\>".*/'username' "\=\>" 'okvpn'/g" application/config/parameters.php;
-        sed -i "s/'password'\s"\=\>".*/'password' "\=\>" '123456'/g" application/config/parameters.php;
-        sed -i "s/'database'\s"\=\>".*/'database' "\=\>" '$DB_NAME'/g" application/config/parameters.php;
+        sed -i "s/'username'\s"\=\>".*/'username' "\=\>" 'okvpn',/g" application/config/parameters.php;
+        sed -i "s/'password'\s"\=\>".*/'password' "\=\>" '123456',/g" application/config/parameters.php;
+        sed -i "s/'database'\s"\=\>".*/'database' "\=\>" '$DB_NAME',/g" application/config/parameters.php;
         export PGPASSWORD='123456';
         psql -U okvpn -h 127.0.0.1 -c "DROP SCHEMA IF EXISTS public CASCADE";
         psql -U okvpn -h 127.0.0.1 -c "CREATE SCHEMA public";
