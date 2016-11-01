5dbe98e33d8cdd5f9ff749b09579915a3b090dd1
Jurasikt <tsykun314@gmail.com>
2016-11-02 00:28:26 +0300

    OK-63: auto deploy - updated test deploy script

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,12 +2,9 @@
 
 export UK2_SSH_KEY
 
-chmod 600 ssh.key;
-rm ssh.key;
-
 echo "$UK2_SSH_KEY" > ssh.key;
 chmod 400 ssh.key;
-ssh root@uk2.yandex.ovh -i ssh.key <<'SSH'
+ssh -o StrictHostKeyChecking=no root@uk2.yandex.ovh -i ssh.key <<'SSH'
 mkdir test
 ls -l
 SSH
